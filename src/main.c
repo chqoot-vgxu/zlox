4047b8afb67e4aecef4170f2a28bb8a55d0be03e
Matteo Zanellato <matteo.zanellato0698@gmail.com>
2021-07-31 18:44:49 +0200

    add stack to VM

--- src/main.c
+++ src/main.c
@@ -1,15 +1,18 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main() {
+    initVM();
     Chunk chunk;
     initChunk(&chunk);
     int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, LOAD_CONST, 123);
     writeChunk(&chunk, constant, 123);
     writeChunk(&chunk, RETURN, 123);
-    disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
